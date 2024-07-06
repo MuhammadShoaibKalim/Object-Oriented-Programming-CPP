391e56a6c6d4ddb87cda870a3de6ae17ddbcff11
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2024-07-06 23:08:16 +0500

    placing deposit, withdraw and getbalance function and constructor

--- Mini-projects/19-ATM-Mini-Project.cpp
+++ Mini-projects/19-ATM-Mini-Project.cpp
@@ -1,15 +1,31 @@
 /*
 Consider an ATM where you can perform following function 🙂
 1- Check Balance
-2-  Cash withdraw
+2- Cash withdraw
 3- Cash Deposit
 4- Exit
-
 */
 
 #include <iostream>
 using namespace std;
+class ATM{
+     private:
+       double balance;
+     public:
+       ATM (double b):balance(b) {
+        double b=balance;
+       };
+      double get_balance()
+      {
+        return balance;
+      } 
 
+      void deposit()
+       {
+        
+       }
+       
+};
 int main() {
    
     
