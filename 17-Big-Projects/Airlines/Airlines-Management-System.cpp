142acb66e46a352166afe188d1d483081782166b
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2024-07-11 23:12:16 +0500

    Adding constructor

--- 17-Big-Projects/Airlines/Airlines-Management-System.cpp
+++ 17-Big-Projects/Airlines/Airlines-Management-System.cpp
@@ -3,7 +3,16 @@
 #include <sstream>
 #include<windns.h>
 using namespace std;
-
+class Airline{
+    private:
+        string destination;
+        string flight;
+        string departure;
+        int  seats;
+    public:
+         Airline(string dep, string f, string des): destination(dep), departure(dep), flight(f) {};
+            
+};
 int main() {
     
 
