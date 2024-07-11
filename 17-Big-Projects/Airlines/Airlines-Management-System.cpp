7a65ff8eea0b6c151da87a47f93af7c8586f6793
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2024-07-12 00:49:06 +0500

    Adding  setter/getter function

--- 17-Big-Projects/Airlines/Airlines-Management-System.cpp
+++ 17-Big-Projects/Airlines/Airlines-Management-System.cpp
@@ -11,7 +11,39 @@ class Airline{
         int  seats;
     public:
          Airline(string dep, string f, string des): destination(dep), departure(dep), flight(f) {};
-            
+     string set_dep( string departure)
+     {
+          this->departure=departure;
+     } 
+     string get_dep () const
+     {
+         return departure;
+     }   
+      string set_des(string destination)
+       {
+         this -> destination=destination;
+       }  
+     string get_des() const 
+       {
+         return destination;
+       } 
+       string set_flight(string flight)
+       {
+         this->flight=flight;
+       }
+       string get_flight() const
+       {
+         return flight;
+       }
+
+       int set_seats(int seats)
+       {
+            this -> seats= seats;
+       }
+         int get_seats() const
+       {
+            return seats;
+       }
 };
 int main() {
     
