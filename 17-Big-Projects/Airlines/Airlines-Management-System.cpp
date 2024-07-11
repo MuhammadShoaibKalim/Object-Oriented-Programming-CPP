2af44c03515a9a83839024b46337842d73a430ee
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2024-07-12 01:08:51 +0500

    File created and saved data successfully

--- 17-Big-Projects/Airlines/Airlines-Management-System.cpp
+++ 17-Big-Projects/Airlines/Airlines-Management-System.cpp
@@ -1,53 +1,64 @@
 #include <iostream>
-#include<fstream>
-#include <sstream>
-#include<windns.h>
+#include <fstream>
 using namespace std;
-class Airline{
-    private:
-        string destination;
-        string flight;
-        string departure;
-        int  seats;
-    public:
-         Airline(string dep, string f, string des): destination(dep), departure(dep), flight(f) {};
-     string set_dep( string departure)
-     {
-          this->departure=departure;
-     } 
-     string get_dep () const
-     {
-         return departure;
-     }   
-      string set_des(string destination)
-       {
-         this -> destination=destination;
-       }  
-     string get_des() const 
-       {
-         return destination;
-       } 
-       string set_flight(string flight)
-       {
-         this->flight=flight;
-       }
-       string get_flight() const
-       {
-         return flight;
-       }
 
-       int set_seats(int seats)
-       {
-            this -> seats= seats;
-       }
-         int get_seats() const
-       {
-            return seats;
-       }
+class Airline {
+private:
+    string destination;
+    string flight;
+    string departure;
+    int seats;
+public:
+    Airline(string dep, string f, string des, int s): departure(dep), flight(f), destination(des), seats(s) {};
+
+    void set_dep(string departure) {
+        this->departure = departure;
+    } 
+
+    string get_dep() const {
+        return departure;
+    }
+
+    void set_des(string destination) {
+        this->destination = destination;
+    }
+
+    string get_des() const {
+        return destination;
+    } 
+
+    void set_flight(string flight) {
+        this->flight = flight;
+    }
+
+    string get_flight() const {
+        return flight;
+    }
+
+    void set_seats(int seats) {
+        this->seats = seats;
+    }
+
+    int get_seats() const {
+        return seats;
+    }
 };
-int main() {
-    
 
-    
+int main() {
+    Airline flight1("F01", "Lahore", "Pakistan", 50);
+    Airline flight2("F02", "Lahore", "Pakistan", 51);
+    Airline flight3("F03", "Islamabad", "Pakistan", 50);
+
+    ofstream out("Flight.txt");
+    if (!out) {
+        cout << "File can't be opened...";
+    } else {
+        out << flight1.get_flight() << " : " << flight1.get_des() << " : " << flight1.get_dep() << " : " << flight1.get_seats() << endl;
+        out << flight2.get_flight() << " : " << flight2.get_des() << " : " << flight2.get_dep() << " : " << flight2.get_seats() << endl;
+        out << flight3.get_flight() << " : " << flight3.get_des() << " : " << flight3.get_dep() << " : " << flight3.get_seats() << endl;
+        cout << "File saved successfully...";
+        out.close();
+    }
+
     return 0;
 }
